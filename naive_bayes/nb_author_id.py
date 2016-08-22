a2bef5192f53e649dc5d444940cb62bded96ee40
Calvin Low <calsaviour@gmail.com>
2016-08-22 23:10:15 +0800

    Corrected the parameter for score

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -34,7 +34,7 @@ clf = GaussianNB()
 t0 = time()
 clf.fit(features_train, labels_train)
 print "training time;", round(time()-t0,3), "s"
-score = clf.score(features_train, labels_train)
+score = clf.score(features_test, labels_test)
 print score
 
 print "Start the prediction"
